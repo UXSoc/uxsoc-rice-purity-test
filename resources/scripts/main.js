b8cf840f9c7c4ba2c3ef029522dd578ca95af1a5
Harvey Sison <harveyjaysison@gmail.com>
2021-04-18 22:29:37 +0800

    changes

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -2,6 +2,7 @@ const form = document.querySelector('#questionnaire');
 const short = document.querySelector('.landing-wrapper-short');
 const long = document.querySelector('.landing-wrapper-long');
 const score = document.querySelector('#score');
+const reaction = document.querySelector('#reaction')
 
 function onSubmit() {
     form.style.display = "none";
@@ -9,6 +10,16 @@ function onSubmit() {
     long.style.display = "flex";
     const checkedLength = document.querySelectorAll('input[type="checkbox"]:checked').length;
     score.innerHTML = checkedLength;
+
+    if (checkedLength >= 31) {
+        reaction.innerHTML = "You're starting it right! You have a journey ahead of you and we're happy to be part of it."
+    } else if (checkedLength >= 21) {
+        reaction.innerHTML = "Growth check! Work those creative muscles. You're doing great fam, we're rooing for you!"
+    } else if (checkedLength >= 11) {
+        reaction.innerHTML = "Wow, you're certified UX learner! Bob Ross must be paiting yet another masterpiece in you."
+    } else {
+        reaction.innerHTML = "Talented, brilliant, incredible, amazing, show stopping, spectacular, never the same, totally unique, completely not ever been done before - you live and breather UX."
+    }
 }
 
 function onAgain() {
